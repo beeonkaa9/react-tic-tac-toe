6ccf0bca01bed497e535aa2002bc8dbd88ddc57c
Bianca Lujan <bblanc545@aol.com>
2021-11-08 09:01:15 -0700

    ensure that spaces with letters cannot be changed once set

--- src/Square.tsx
+++ src/Square.tsx
@@ -7,29 +7,32 @@ type SquareProps = {
 
 export function Square({ turn, handleClick }: SquareProps): JSX.Element {
   const [letter, setLetter] = useState('')
-  // let isClicked = false
+  const [isClicked, setIsClicked] = useState(false)
 
-  function buttonClick() {
-    // checkButton()
+  //player 1 gets X and player 2 gets O; increase state in Board by one with handleClick()
+  function handleTurn() {
     if (turn === 1 || turn % 2 === 1) {
       setLetter('X')
     } else {
       setLetter('O')
     }
+    setIsClicked(true)
     handleClick()
-    // isClicked = true
   }
 
-  // function checkButton() {
-  //   if (isClicked) {
-  //     alert('you cannot change this square!')
-  //     setLetter(letter)
-  //   }
-  // }
+  //if the button has already been clicked to set a letter, do not change it; otherwise, set the letter
+  function clickButton() {
+    if (isClicked) {
+      alert('You cannot change this square, please select a different one')
+      letter === 'O' ? setLetter('O') : setLetter('X')
+    } else {
+      handleTurn()
+    }
+  }
 
   return (
     <>
-      <button onClick={buttonClick}>{letter}</button>
+      <button onClick={clickButton}>{letter}</button>
     </>
   )
 }
