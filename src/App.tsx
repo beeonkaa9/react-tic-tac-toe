60a3aa4a0635ad877dd4c463ee38d03f2b2419e7
Bianca Lujan <bblanc545@aol.com>
2021-11-23 11:10:11 -0700

    added pointer cursor in styling; variable name changes and functions moved inline in Board

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,12 @@ import { Board } from './components/Board'
 
 function App() {
   return (
-    <>
-      <div className="App">
-        <header className="AppHeader">
-          <h1>Tic Tac Toe!</h1>
-        </header>
-        <Board />
-      </div>
-    </>
+    <div className="App">
+      <header className="AppHeader">
+        <h1>Tic Tac Toe!</h1>
+      </header>
+      <Board />
+    </div>
   )
 }
 
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -6,69 +6,63 @@ import { Status } from './Status'
 
 export function Board(): JSX.Element {
   const [turn, setTurn] = useState(0)
-  const [boardState, setBoardState] = useState([
+  const [gameHistory, setGameHistory] = useState([
     {
       board: ['', '', '', '', '', '', '', '', ''],
     },
   ])
-  const [playerOne, setPlayerOne] = useState(true)
+  const [isplayerOnesTurn, setIsPlayerOnesTurn] = useState(true)
 
   /*
     access boardState to record player's moves and save them into array;
     this accesses the board property within boardState for each turn
   */
-  const currentBoardState = boardState.slice(0, turn + 1)
+  const currentBoardState = gameHistory.slice(0, turn + 1)
   const currentMove = currentBoardState[currentBoardState.length - 1]
   const board = currentMove.board.slice(0)
   const winner = determineWin(board)
 
-  function handleTurn(index: number) {
-    if (determineWin(board)) {
-      return board[index]
-    }
-
-    setTurn(turn + 1)
-
-    playerOne ? (board[index] = 'X') : (board[index] = 'O')
-
-    playerOne ? setPlayerOne(false) : setPlayerOne(true)
-
-    setBoardState([...boardState, { board: board }])
-  }
-
-  /*
-    to access previous moves and undo them; goes to index of turn
-    requested, access the board property there, and remove the rest of the arrays
-    in boardState that are after the turn's index
-  */
-  function changeTurn(turn: number) {
-    const newBoard = boardState.slice(0, turn + 1)
-    setTurn(turn)
-    turn % 2 === 0 ? setPlayerOne(true) : setPlayerOne(false)
-
-    setBoardState([...newBoard])
-  }
-
   return (
     <>
       <div className="gameStatus">
-        <Status board={board} playerOne={playerOne} winner={winner} />
+        <Status board={board} playerOne={isplayerOnesTurn} winner={winner} />
       </div>
       <div className="gameSpace">
         <div className="board">
-          {boardState[boardState.length - 1].board.map((squares, i) => (
+          {gameHistory[gameHistory.length - 1].board.map((squares, i) => (
             <Square
               key={i}
               index={i}
               turn={turn}
-              handleTurn={(index) => handleTurn(index)}
+              handleTurn={(index) => {
+                if (determineWin(board)) {
+                  return board[index]
+                }
+                setTurn(turn + 1)
+                isplayerOnesTurn ? (board[index] = 'X') : (board[index] = 'O')
+                isplayerOnesTurn
+                  ? setIsPlayerOnesTurn(false)
+                  : setIsPlayerOnesTurn(true)
+                setGameHistory([...gameHistory, { board: board }])
+              }}
               board={board}
             />
           ))}
         </div>
 
         <div className="gameMoves">
-          <Moves boardState={boardState} changeTurn={changeTurn} />
+          <Moves
+            boardState={gameHistory}
+            changeTurn={(turn) => {
+              const newBoard = gameHistory.slice(0, turn + 1)
+              setTurn(turn)
+              turn % 2 === 0
+                ? setIsPlayerOnesTurn(true)
+                : setIsPlayerOnesTurn(false)
+
+              setGameHistory([...newBoard])
+            }}
+          />
         </div>
       </div>
     </>
